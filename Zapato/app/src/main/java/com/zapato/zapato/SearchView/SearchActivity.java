184d20dbb015ee47e5f44010a6f4b9b5874db7fa
Helen Pabst <hlnpabst@gmail.com>
2018-04-07 17:45:19 -0700

    Searchable.xml, searchable activity, search interface

--- Zapato/app/src/main/java/com/zapato/zapato/SearchView/SearchActivity.java
+++ Zapato/app/src/main/java/com/zapato/zapato/SearchView/SearchActivity.java
@@ -1,15 +1,30 @@
 package com.zapato.zapato.SearchView;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.zapato.zapato.R;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends ListActivity {
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_search);
+
+      // Get the intent, verify the action and get the query
+      Intent intent = getIntent();
+      if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+         String query = intent.getStringExtra(SearchManager.QUERY);
+         doMySearch(query);
+      }
+
+   }
+
+   private void doMySearch(String query)
+   {
+      //TODO: search data: https://developer.android.com/guide/topics/search/search-dialog.html
    }
 }
