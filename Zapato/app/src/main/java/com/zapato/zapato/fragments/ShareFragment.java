baddfe9c7f3adc6205ad5ad765e7b4b0f64b0106
Agustin Barajas <agustincards14@gmail.com>
2018-05-02 00:59:53 -0700

    Created basic upload photo function and edit New Post title

--- Zapato/app/src/main/java/com/zapato/zapato/fragments/ShareFragment.java
+++ Zapato/app/src/main/java/com/zapato/zapato/fragments/ShareFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.design.widget.TextInputEditText;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +29,8 @@ public class ShareFragment extends BaseFragment{
     private ImageView photo2;
     private ImageView photo3;
     private ImageView photo4;
+    private TextInputEditText titleText;
+    int pictureCount = 0;
 
 
     private ArrayList<ImageView> shoe_photos;
@@ -48,22 +51,39 @@ public class ShareFragment extends BaseFragment{
         View view = inflater.inflate(R.layout.fragment_share_1, container, false);
         ButterKnife.bind(this, view);
 
+
+
+        titleText = view.findViewById(R.id.edit_title);
         photo1= view.findViewById(R.id.imageView1);
-
-        view.findViewById(R.id.takephoto_button).setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {
-            dispatchTakePictureIntent();
-         }
-        });
-
+        photo2= view.findViewById(R.id.imageView2);
+        photo3= view.findViewById(R.id.imageView3);
+//        photo4= view.findViewById(R.id.imageView4);
+        // TODO : Fix thumbnail rotation issue when pictures taken
+        // TODO : Make ImageViews of taken pictures deletable when clicked
+        // TODO : Save full-size taken pictures into Zapato DB, then access them from Product Detail page
+        pictureButtonListen(view);
 
         return view;
+    }
+
+    public void pictureButtonListen(View view){
+        view.findViewById(R.id.takephoto_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private void dispatchTakePictureIntent() {
+        pictureCount++;
+        if (pictureCount>4) {
+            getView().findViewById(R.id.takephoto_button).setClickable(false);
+            return;
+        }
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -75,7 +95,18 @@ public class ShareFragment extends BaseFragment{
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            photo1.setImageBitmap(imageBitmap);
+            switch (pictureCount) {
+                case 1: photo1.setImageBitmap(imageBitmap); photo1.setRotation(90);
+                break;
+                case 2: photo2.setImageBitmap(imageBitmap); photo2.setRotation(90);
+                break;
+                case 3: photo3.setImageBitmap(imageBitmap); photo3.setRotation(90);
+                    break;
+//                case 4: photo4.setImageBitmap(imageBitmap); photo4.setRotation(90);
+//                    break;
+                default:
+                    break;
+            }
         }
     }
 
